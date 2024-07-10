d935fd0159812ff916d717c845ad4184dfd44838
Nigel Horne <njh@bandsman.co.uk>
2024-07-10 08:54:00 -0400

    Release 0.09

--- lib/DateTime/Format/Genealogy.pm
+++ lib/DateTime/Format/Genealogy.pm
@@ -46,11 +46,11 @@ DateTime::Format::Genealogy - Create a DateTime object from a Genealogy Date
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
